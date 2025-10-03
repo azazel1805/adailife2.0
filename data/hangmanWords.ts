cb284d8f9dc8cd95dcc3bccfe87ab676ef210b8e
Onur Tosuner <128583602+azazel1805@users.noreply.github.com>
2025-10-03 17:58:00 +0300

    Create hangmanWords.ts

---
+++ data/hangmanWords.ts
@@ -0,0 +1,15 @@
+export const hangmanWords: { [key: string]: string[] } = {
+  'Kolay': [
+    'CAT', 'DOG', 'SUN', 'SKY', 'RED', 'BLUE', 'RUN', 'EAT', 'BIG', 'HOT',
+    'BALL', 'TREE', 'BOOK', 'FISH', 'HAND', 'LOVE', 'RAIN', 'SONG', 'STAR', 'WATER'
+  ],
+  'Orta': [
+    'APPLE', 'BEACH', 'CHAIR', 'EARTH', 'FRIEND', 'GREEN', 'HAPPY', 'HOUSE', 'MUSIC', 'OCEAN',
+    'PAPER', 'PARTY', 'PHONE', 'RIVER', 'SMILE', 'STORE', 'TABLE', 'TIGER', 'WHITE', 'WOMAN'
+  ],
+  'Zor': [
+    'BEAUTIFUL', 'CHALLENGE', 'COMPUTER', 'DIFFICULT', 'EDUCATION', 'ENVIRONMENT', 'EXCELLENT',
+    'GOVERNMENT', 'IMPORTANT', 'KNOWLEDGE', 'LANGUAGE', 'OPPORTUNITY', 'RESTAURANT',
+    'SCIENTIST', 'TECHNOLOGY', 'UNDERSTAND', 'UNIVERSITY', 'WONDERFUL', 'YESTERDAY', 'CHOCOLATE'
+  ]
+};
