5f484903ad8286afd766e8e88087886cefb67cc7
Onur Tosuner <128583602+azazel1805@users.noreply.github.com>
2025-10-07 17:03:40 +0300

    Update Icons.tsx

--- components/icons/Icons.tsx
+++ components/icons/Icons.tsx
@@ -57,6 +57,8 @@ export const OutlineIcon: React.FC = () => <span role="img" aria-label="Outline 
 // FIX: Add missing VerbToBeIcon to resolve import error in Basics.tsx.
 export const VerbToBeIcon: React.FC = () => <span role="img" aria-label="Verb to be icon" className="text-xl">🧱</span>;
 export const BasicsIcon: React.FC = () => <span role="img" aria-label="Basics icon" className="text-xl">🧱</span>;
+export const QAIcon: React.FC = () => <span role="img" aria-label="Question Answer icon" className="text-xl">❓</span>;
+
 
 
 
